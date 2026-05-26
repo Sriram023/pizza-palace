ea0e3ec7f7324223198e7bc14f591f253f6818d4
Sriram Ravichandran <sriramravichandran2006@gmail.com>
2026-05-26 22:51:44 +0530

    Log token in axios request interceptor
    Add logging for the token in the request interceptor.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-
   baseURL: import.meta.env.VITE_API_URL,
-
   withCredentials: true,
 });
 
@@ -11,8 +9,9 @@ api.interceptors.request.use((config) => {
 
   const token = localStorage.getItem('token');
 
-  if (token) {
+  console.log('TOKEN:', token);
 
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
