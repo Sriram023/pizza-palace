65ea5d9259b1bc82f8e6dab3190227e8b99198c6
Sriram Ravichandran <sriramravichandran2006@gmail.com>
2026-05-26 16:03:21 +0530

    Add baseURL and withCredentials to axios instance

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
+
   baseURL: import.meta.env.VITE_API_URL,
+
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
@@ -9,6 +12,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
 
   if (token) {
+
     config.headers.Authorization = `Bearer ${token}`;
   }
 
